24f76ddc771b42a1eb3614c0f4481f37986e8d8c
Suresh Pawar <67982666+sureshdpawar@users.noreply.github.com>
2023-12-14 20:43:57 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,38 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
 import streamlit as st
 
-# Header Section
-st.title("AI/ML Project Portfolio")
-st.subheader("Welcome to my portfolio!")
+"""
+# Welcome to Streamlit!
 
-# Navigation Bar
-nav_selection = st.sidebar.radio("Navigation", ["Home", "Projects", "About", "Contact"])
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-if nav_selection == "Home":
-    st.write("This is the home page.")
-    # Add content specific to the home page
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
 
-elif nav_selection == "Projects":
-    st.subheader("AI/ML Projects")
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
 
-    # Project 1
-    st.subheader("Project 1")
-    st.image("project1_image.jpg", caption="Project 1", use_column_width=True)
-    st.write("Project 1 description.")
-    st.button("View Details")
 
-    # Project 2
-    st.subheader("Project 2")
-    st.image("project2_image.jpg", caption="Project 2", use_column_width=True)
-    st.write("Project 2 description.")
-    st.button("View Details")
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    # Add more projects as needed
+    Point = namedtuple('Point', 'x y')
+    data = []
 
-elif nav_selection == "About":
-    st.subheader("About Me")
+    points_per_turn = total_points / num_turns
 
-    # Add information about yourself, your skills, and background
+    for curr_point_num in range(total_points):
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+        radius = curr_point_num / total_points
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
+        data.append(Point(x, y))
 
-elif nav_selection == "Contact":
-    st.subheader("Contact Me")
-
-    # Add contact information such as email, social media links, etc.
-
-# Footer Section
-st.markdown("---")
-st.write("© 2023 My Portfolio. All rights reserved.")
-st.write("Connect with me on [GitHub](https://github.com/your_username) | [LinkedIn](https://linkedin.com/in/your_profile)")
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
